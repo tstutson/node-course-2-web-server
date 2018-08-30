73f40463a2fc13343d7d203990818c3db8e58fe9
Tyler Stutson <tyler@pactsafe.com>
2018-08-30 14:07:22 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -53,6 +54,6 @@ app.get('/bad', function (req, res) {
     })
 });
 
-app.listen(3000, function () {
-    console.log('Server is up on 3000.');
+app.listen(port, function () {
+    console.log(`Server is up on ${port}.`);
 });
