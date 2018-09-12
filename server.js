0adbd768e4b9dc53748b5141ca8c1bcf2dc2f7c8
Tyler Stutson <tyler@pactsafe.com>
2018-09-12 14:52:52 -0400

    haaahahaahaha

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ function bootstrapEmbeddedURL(req, res, next) {
     if (!req.context) req.context = {};
     var client_id = '58dc305a75089c733ebc8487';
     var access_token = 'WpkFloEPcFj3OWSAMCoebBYlchW0zA5Ie7XjPmQUBCo_';
-    var revision = '5b99190f94e9292a52fee3a5';
+    var revision = '5b99606294e9292a52fee8c6';
 
     var embedded_editor_url = 'https://dev-api.pactsafe.com/v1.1/revisions/' + revision + '/embedded_url?state=something';
 
